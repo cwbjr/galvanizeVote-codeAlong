44bfd27aeb69e70ee1b0156af1a54333e7358ba3
Charles Ballew <charleswballewjr@gmail.com>
2018-03-26 11:37:10 -0600

    catch up to code along

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,23 +1,48 @@
 <template>
-  <div class="sign-in">
-    <img src="https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8wNjAvMzU4L2kwMi9sb29raW5nLUNhbHZpbi1Ib2JiZXNfZmYuanBn">
-
-  </div>
+<div class="sign-in">
+  <header>
+    <!-- <img src="https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8wNjAvMzU4L2kwMi9sb29raW5nLUNhbHZpbi1Ib2JiZXNfZmYuanBn" /> -->
+    <img src="https://www.skidmore.edu/vote/images/vote.jpg" alt="Votes logo" />
+    <h1>Galvanize Votes</h1>
+  </header>
+  <main>
+    <p>The future home of Galvanize Votes</p>
+  </main>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+//    import HelloWorld from "@/components/HelloWorld.vue"
 
 export default {
-  name: 'SignIn',
-  components: {
-    HelloWorld,
-  },
-};
+  name: "SignIn",
+  components: {}
+}
 </script>
 
 <style scoped lang="scss">
-  @import "~@/styles/_colors";
-  @import "~@/styles/_Typography";
+@import "~@/styles/_colors";
+@import "~@/styles/_typography";
+
+.sign-in {
+    @include base-font;
+    header {
+        margin: 2rem 0;
+        text-align: center;
+        h1 {
+            @include primary-header-font;
+            text-align: center;
+        }
+        img {
+            max-width: 320px;
+            margin-bottom: 2rem;
+        }
+    }
+    main {
+        p {
+            text-align: center;
+        }
+    }
+}
 </style>
