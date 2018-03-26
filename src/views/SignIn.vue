9bb58f20e6568041df1e4c9b82beaec88c5440ca
Charles Ballew <charleswballewjr@gmail.com>
2018-03-26 11:24:05 -0600

    splash page complete | init setup

---
+++ src/views/SignIn.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sign-in">
+    <img src="https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8wNjAvMzU4L2kwMi9sb29raW5nLUNhbHZpbi1Ib2JiZXNfZmYuanBn">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'SignIn',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "~@/styles/_colors";
+  @import "~@/styles/_Typography";
+</style>
